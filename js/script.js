3fd6aadc7706a95ef1c249af9c3c438a0d1f17b4
tnromy <tuanromy@gmail.com>
2020-02-15 16:20:14 +0700

    add skills & tools with collapsible menu and progress bar

--- js/script.js
+++ js/script.js
@@ -7,10 +7,24 @@ $(document).ready(function(){
 		$(dest).attr(attrName, src);
 	}
 
+	function progresBar(barName, valNow){
+		var barHtml = '<div><label>'+barName+'</label></div><div class="progress mb-4"><div class="progress-bar" role="progressbar" style="width: '+valNow+'%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">'+valNow+'%</div></div>';
+
+		return barHtml;
+	}
+
+	function eachBar(id, data){
+		$.each(data, function(index, value){
+			$(id).append(progresBar(value.name, value.value));
+		});
+	}
+
 	$.getJSON('data.txt',
 	function (data, textStatus, jqXHR){
 		
-		putData(data.fullName, '#cv-full-name');
+		putData(data.fullName, '.cv-full-name');
+
+		putData(data.born, '.cv-born');
 
 		putAttrVal('src', data.ava, '#cv-ava');
 
@@ -23,6 +37,20 @@ $(document).ready(function(){
 		putAttrVal('href', data.linkLinkedin, '#cv-social-linkedin');
 
 		putAttrVal('value', data.email, '#email');
+
+		// skills & tools
+
+		eachBar('#cv-electro-skills', data.electroSkills);
+
+		eachBar('#cv-electro-tools', data.electroTools);
+
+		eachBar('#cv-server-skills', data.serverSkills);
+
+		eachBar('#cv-server-tools', data.serverTools);
+
+		eachBar('#cv-programing-skills', data.programingLang);
+
+		eachBar('#cv-programing-tools', data.programingFramework);
 
 
 	});
